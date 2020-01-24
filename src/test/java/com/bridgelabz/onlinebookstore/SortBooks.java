3437d6949400cdf2b3eb8bf4ae80af0cfe2d18aa
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2020-01-24 12:27:57 +0530

    Added test case for sort book API

--- src/test/java/com/bridgelabz/onlinebookstore/SortBooks.java
+++ src/test/java/com/bridgelabz/onlinebookstore/SortBooks.java
@@ -1,11 +1,3 @@
-/******************************************************************************
- *
- *  Author  Deepali Kalagate and Shivanjali Sangale
- *  Purpose: Test for Search Book by Author and BookName .
- *
- *  @since 23-01-2020
- *
- ******************************************************************************/
 package com.bridgelabz.onlinebookstore;
 
 import io.restassured.RestAssured;
@@ -19,17 +11,17 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
 
-public class SearchBook extends BaseTest {
+public class SortBooks extends BaseTest {
 
-    //search book by author
+    //sort by price in ascending order
     @Test
-    public void givenListOfBooks_WhenSearchByCorrectFirstAuthor_ThenShouldReturnBookByAuthor() {
+    public void givenListOfBooks_WhenSortByPrice_ThenShouldReturnBookInAscendingOrderOfPrice() {
         try {
             Response response = RestAssured.given()
                     .contentType(ContentType.JSON)
                     .accept(ContentType.JSON)
                     .when()
-                    .get("searchByAuthorOrTitle?searchElement=Chetan Bhagat");
+                    .get("sortByPrice?sortType=price");
             int status = response.getStatusCode();
             ResponseBody body = response.getBody();
             Object Object = new JSONParser().parse(body.prettyPrint());
@@ -40,82 +32,13 @@ public class SearchBook extends BaseTest {
     }
 
     @Test
-    public void givenListOfBooks_WhenSearchByCorrectSecondAuthor_ThenShouldReturnBookByAuthor() {
+    public void givenListOfBooks_WhenSortByPriceProvideAnyNumber_ThenShouldThrowException() {
         try {
             Response response = RestAssured.given()
                     .contentType(ContentType.JSON)
                     .accept(ContentType.JSON)
                     .when()
-                    .get("searchByAuthorOrTitle?searchElement=Dan Brown");
-            int status = response.getStatusCode();
-            ResponseBody body = response.getBody();
-            Object Object = new JSONParser().parse(body.prettyPrint());
-            MatcherAssert.assertThat(status, Matchers.equalTo(HttpStatus.SC_OK));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void givenListOfBooks_WhenSearchByIncorrectAuthor_ThenShouldThrowException() {
-        try {
-            Response response = RestAssured.given()
-                    .contentType(ContentType.JSON)
-                    .accept(ContentType.JSON)
-                    .when()
-                    .get("searchByAuthorOrTitle?searchElement=SaneGuruji");
-            int status = response.getStatusCode();
-            ResponseBody body = response.getBody();
-            Object Object = new JSONParser().parse(body.prettyPrint());
-            MatcherAssert.assertThat(status, Matchers.equalTo(HttpStatus.SC_INTERNAL_SERVER_ERROR));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //search book by bookName
-    @Test
-    public void givenListOfBooks_WhenSearchByCorrectFirstBookName_ThenShouldReturnBookByBookName() {
-        try {
-            Response response = RestAssured.given()
-                    .contentType(ContentType.JSON)
-                    .accept(ContentType.JSON)
-                    .when()
-                    .get("searchByAuthorOrTitle?searchElement=Angels And Demons");
-            int status = response.getStatusCode();
-            ResponseBody body = response.getBody();
-            Object Object = new JSONParser().parse(body.prettyPrint());
-            MatcherAssert.assertThat(status, Matchers.equalTo(HttpStatus.SC_OK));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void givenListOfBooks_WhenSearchByCorrectSecondBookName_ThenShouldReturnBookByBookName() {
-        try {
-            Response response = RestAssured.given()
-                    .contentType(ContentType.JSON)
-                    .accept(ContentType.JSON)
-                    .when()
-                    .get("searchByAuthorOrTitle?searchElement=The Da Vinci Code");
-            int status = response.getStatusCode();
-            ResponseBody body = response.getBody();
-            Object Object = new JSONParser().parse(body.prettyPrint());
-            MatcherAssert.assertThat(status, Matchers.equalTo(HttpStatus.SC_OK));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void givenListOfBooks_WhenSearchByIncorrectBookName_ThenShouldThrowException() {
-        try {
-            Response response = RestAssured.given()
-                    .contentType(ContentType.JSON)
-                    .accept(ContentType.JSON)
-                    .when()
-                    .get("searchByAuthorOrTitle?searchElement=The Times Of India");
+                    .get("sortByPrice?sortType=400");
             int status = response.getStatusCode();
             ResponseBody body = response.getBody();
             Object Object = new JSONParser().parse(body.prettyPrint());
@@ -126,13 +49,66 @@ public class SearchBook extends BaseTest {
     }
 
     @Test
-    public void givenListOfBooks_WhenSearchByInvalidBookName_ThenShouldThrowException() {
+    public void givenListOfBooks_WhenSortByPriceProvideAlphanumericValue_ThenShouldThrowException() {
         try {
             Response response = RestAssured.given()
                     .contentType(ContentType.JSON)
                     .accept(ContentType.JSON)
                     .when()
-                    .get("searchByAuthorOrTitle?searchElement=9874");
+                    .get("sortByPrice?sortType=550/-");
+            int status = response.getStatusCode();
+            ResponseBody body = response.getBody();
+            Object Object = new JSONParser().parse(body.prettyPrint());
+            MatcherAssert.assertThat(status, Matchers.equalTo(HttpStatus.SC_INTERNAL_SERVER_ERROR));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //sort books by title
+    @Test
+    public void givenListOfBooks_WhenSortByTitle_ThenShouldReturnBooksByTitle() {
+        try {
+            Response response = RestAssured.given()
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                    .when()
+                    .get(" sortByPrice?sortType=title");
+            int status = response.getStatusCode();
+            ResponseBody body = response.getBody();
+            Object Object = new JSONParser().parse(body.prettyPrint());
+            MatcherAssert.assertThat(status, Matchers.equalTo(HttpStatus.SC_OK));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenListOfBooks_WhenSortByTitleByAlphanumericValue_ThenShouldThrowException() {
+        try {
+            Response response = RestAssured.given()
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                    .when()
+                    .get(" sortByPrice?sortType=11/22/63");
+            int status = response.getStatusCode();
+            ResponseBody body = response.getBody();
+            Object Object = new JSONParser().parse(body.prettyPrint());
+            MatcherAssert.assertThat(status, Matchers.equalTo(HttpStatus.SC_INTERNAL_SERVER_ERROR));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void givenListOfBooks_WhenNoValueProvidedForSort_ThenShouldThrowException() {
+        try {
+            Response response = RestAssured.given()
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                    .when()
+                    .get(" sortByPrice?sortType=");
             int status = response.getStatusCode();
             ResponseBody body = response.getBody();
             Object Object = new JSONParser().parse(body.prettyPrint());
